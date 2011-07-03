595aaefd8bfd5c20f2f26c98de859d0c5b72038d
unknown <pranavn@.corporate.thoughtworks.com>
2011-07-03 20:39:48 +0530

    first  refactor: extract method and rename

--- src/Customer.java
+++ src/Customer.java
@@ -32,23 +32,9 @@ public class Customer {
 			Rental each = (Rental) rentals.nextElement();
 			
 			//determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2) 
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
-			
-			// add frequent renter points
+            thisAmount = amountFor(each);
+
+            // add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
 			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
@@ -65,4 +51,24 @@ public class Customer {
 		
 		return result;
 	}
+
+    private double amountFor(Rental rental) {
+        double rentalAmount = 0;
+        switch (rental.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+            rentalAmount += 2;
+            if (rental.getDaysRented() > 2)
+                rentalAmount += (rental.getDaysRented() - 2) * 1.5;
+            break;
+        case Movie.NEW_RELEASE:
+            rentalAmount += rental.getDaysRented() * 3;
+            break;
+        case Movie.CHILDRENS:
+            rentalAmount += 1.5;
+            if (rental.getDaysRented() > 3)
+                rentalAmount += (rental.getDaysRented() - 3) * 1.5;
+            break;
+        }
+        return rentalAmount;
+    }
 }
