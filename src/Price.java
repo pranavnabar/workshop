84e09d409448665882b5e41eced64e72888bdc72
Pranav Nabar <pranavn@thoughtworks.com>
2011-07-03 22:40:34 +0530

    replace conditional with polymorphism for frequentRenterPoints

--- src/Price.java
+++ src/Price.java
@@ -2,4 +2,8 @@ public abstract class Price {
     abstract int getPriceCode();
 
     abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
