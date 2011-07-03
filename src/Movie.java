a6e42a27af1c52922d2acd4273f9d9af5e64d9d1
Pranav Nabar <pranavn@thoughtworks.com>
2011-07-03 22:11:16 +0530

    move method getFrequentRenterPoints to Movie: switch on someone else's attribute

--- src/Movie.java
+++ src/Movie.java
@@ -46,4 +46,10 @@ public class Movie {
         }
         return rentalAmount;
     }
+
+    int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == NEW_RELEASE) && daysRented > 1)
+            return 2;
+        return 1;
+    }
 }
--- src/Rental.java
+++ src/Rental.java
@@ -6,25 +6,24 @@
  */
 public class Rental {
 
-	private Movie _movie;
-	private int _daysRented;
-	
-	public Rental(Movie movie, int daysRented) {
-		_movie = movie;
-		_daysRented = daysRented;
-	}
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
 
     public Movie getMovie() {
-		return _movie;
-	}
+        return _movie;
+    }
 
     double getCharge() {
         return _movie.getCharge(_daysRented);
     }
 
     int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && _daysRented > 1)
-            return 2;
-        return 1;
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
+
 }
